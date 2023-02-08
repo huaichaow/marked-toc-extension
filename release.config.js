2fbea27c6d35d8ab25b5019aafff7056ff105de7
Huaichao Wang <huaichao.wang@qq.com>
2023-02-08 11:18:17 +0800

    refactor: update files to publish to github releases

--- release.config.js
+++ release.config.js
@@ -4,7 +4,14 @@ module.exports = {
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
       '@semantic-release/npm',
-      '@semantic-release/github',
+			['@semantic-release/github', {
+				assets: [
+					{
+						path: 'lib/**',
+						label: 'bundle files',
+					},
+				],
+			}],
       '@semantic-release/git'
     ]
   };
