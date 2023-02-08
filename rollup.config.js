b69d6509b16ce45a966656d7152a616028a6c42e
Huaichao Wang <huaichao.wang@qq.com>
2023-02-08 10:13:18 +0800

    refactor: bundle to different module types with rollup

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { defineConfig } = require('rollup');
+const ts = require('rollup-plugin-ts');
+
+module.exports = defineConfig([
+	{
+		input: 'src/index.ts',
+		output: {
+			file: 'lib/marked-toc-extension.js',
+			format: 'cjs',
+		},
+		plugins: [
+			ts(),
+		],
+	},
+	{
+		input: 'src/index.ts',
+		output: {
+			file: 'lib/marked-toc-extension..esm.js',
+			format: 'esm',
+		},
+		plugins: [
+			ts(),
+		],
+	},
+	{
+		input: 'src/index.ts',
+		output: {
+			file: 'lib/marked-toc-extension.umd.js',
+			format: 'umd',
+			name: 'markedTocExtension',
+		},
+		plugins: [
+			ts(),
+		],
+	},
+]);
