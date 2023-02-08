ec03a204f601f0cb6e0be9d657c80a3e2fe2e281
Huaichao Wang <huaichao.wang@qq.com>
2023-02-08 13:58:10 +0800

    style: update indent style

--- release.config.js
+++ release.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-    branches: 'main',
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/npm',
-			['@semantic-release/github', {
-				assets: [
-					{
-						path: 'lib/**',
-					},
-				],
-			}],
-      '@semantic-release/git'
-    ]
-  };
+  branches: 'main',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    ['@semantic-release/github', {
+      assets: [
+        {
+          path: 'lib/**',
+        },
+      ],
+    }],
+    '@semantic-release/git',
+  ],
+};
